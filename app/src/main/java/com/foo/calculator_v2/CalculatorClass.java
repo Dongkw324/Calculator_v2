e8d67fcb8209ef8cdb6a598fba2e3e677d605307
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2021-03-16 17:11:06 +0900

    add function

---
+++ app/src/main/java/com/foo/calculator_v2/CalculatorClass.java
@@ -0,0 +1,84 @@
+package com.foo.calculator_v2;
+
+import java.text.DecimalFormat;
+
+public class CalculatorClass {
+
+    public static final String CLEAR_INPUT = "0";
+
+    private double resultNumber = 0;
+    private double lastNumber = 0;
+    private String operator = "+";
+    private String operatorStr = "";
+
+    DecimalFormat decimalFormat = new DecimalFormat("###,###.#####");
+
+    public String getDecimal(String number){
+        String temp = number.replace(",", "");
+        return decimalFormat.format(Double.parseDouble(temp));
+    }
+
+    public String getDecimal(Double number){
+        return decimalFormat.format(number);
+    }
+
+    public String getOperatorStr() {
+        return operatorStr;
+    }
+
+
+    public void setClear(){
+         resultNumber = 0;
+         lastNumber = 0;
+         operator = "+";
+         operatorStr = "";
+    }
+    public double doubleResultNumber(double result, double lastInputNumber, String operator){
+        switch(operator){
+            case "+":
+                result += lastInputNumber;
+                break;
+            case "-":
+                result -= lastInputNumber;
+                break;
+            case "*":
+                result *= lastInputNumber;
+                break;
+            case "/":
+                result /= lastInputNumber;
+                break;
+        }
+        return result;
+    }
+
+    public String getResult(boolean firstInput, String getResultNumber, String lastOperator) {
+        if (firstInput) {
+            if(lastOperator.equals("=")){
+                resultNumber = doubleResultNumber(resultNumber, lastNumber, operator);
+                operatorStr="";
+            } else {
+                operator = lastOperator;
+                if(operatorStr.equals("")){
+                    operatorStr = getResultNumber + " " + lastOperator;
+                }
+                else{
+                    operatorStr = operatorStr.substring(0, operatorStr.length() - 1);
+                    operatorStr = operatorStr + lastOperator;
+                }
+            }
+
+        } else {
+            lastNumber = Double.parseDouble(getResultNumber.replace(",", ""));
+            resultNumber = doubleResultNumber(resultNumber, lastNumber, operator);
+            if(lastOperator.equals("=")){
+                operatorStr = "";
+            }
+            else{
+                operatorStr = operatorStr + " " + getResultNumber + " " + lastOperator;
+                operator = lastOperator;
+            }
+        }
+
+        return getDecimal(resultNumber);
+    }
+}
--- app/src/main/java/com/foo/calculator_v2/MainActivity.java
+++ app/src/main/java/com/foo/calculator_v2/MainActivity.java
@@ -2,13 +2,175 @@ package com.foo.calculator_v2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean firstInput = true; //첫 입력은 무조건 true
+
+    ScrollView scrollView;
+    TextView resultTextView, historyTextView;
+    Button clearButton, entryClearButton, backSpaceButton, decimalButton;
+
+    Button[] number_btn = new Button[10];
+    Button[] operator_btn = new Button[5];
+
+    CalculatorClass calculatorClass = new CalculatorClass();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scrollView = findViewById(R.id.scroll_View);
+        resultTextView = findViewById(R.id.result_textView);
+        historyTextView = findViewById(R.id.history_textView);
+
+        clearButton = findViewById(R.id.btn_clear);
+        entryClearButton = findViewById(R.id.btn_entry_clear);
+        backSpaceButton = findViewById(R.id.btn_back_space);
+        decimalButton = findViewById(R.id.btn_decimal);
+
+
+        for (int i = 0; i < number_btn.length; i++) {
+            number_btn[i] = findViewById(R.id.number_btn_0 + i);
+        }
+
+        for (int i = 0; i < operator_btn.length; i++) {
+            operator_btn[i] = findViewById(R.id.operator_0 + i);
+        }
+
+        for(Button button : number_btn){
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    numberClick(view);
+                }
+            });
+        }
+
+        for(Button button: operator_btn){
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    operatorClick(view);
+                }
+            });
+        }
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearClick(view);
+            }
+        });
+
+        entryClearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                entryClearClick(view);
+            }
+        });
+
+        backSpaceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                backSpaceClick(view);
+            }
+        });
+
+        decimalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                decimalClick(view);
+            }
+        });
+    }
+
+    private void decimalClick(View view) {
+        if(firstInput){
+            resultTextView.setTextColor(getResources().getColor(R.color.black));
+            resultTextView.setText("0.");
+            firstInput = false;
+        } else {
+            if(resultTextView.getText().toString().contains(".")){
+                return;
+            } else {
+                resultTextView.append(".");
+            }
+        }
+    }
+
+    private void backSpaceClick(View view) {
+        if (firstInput && !calculatorClass.getOperatorStr().equals("")) {
+            return;
+        } else {
+            if(resultTextView.getText().toString().length() > 1){
+                String getNumber = resultTextView.getText().toString().replace(",", "");
+                String subString = getNumber.substring(0, getNumber.length() -1);
+                String decimalNumber = calculatorClass.getDecimal(subString);
+                resultTextView.setText(decimalNumber);
+            } else{
+                clearData();
+            }
+        }
+    }
+
+    private void entryClearClick(View view) {
+        clearData();
+    }
+
+    private void clearClick(View view) {
+        calculatorClass.setClear();
+        historyTextView.setText(calculatorClass.getOperatorStr());
+        clearData();
+    }
+
+    private void operatorClick(View view) {
+        String getResultNumber = resultTextView.getText().toString();
+        String operator = view.getTag().toString();
+        String getNumber = calculatorClass.getResult(firstInput, getResultNumber, operator);
+        resultTextView.setText(getNumber);
+        historyTextView.setText(calculatorClass.getOperatorStr());
+        firstInput = true;
+    }
+
+    private void numberClick(View view) {
+        if(firstInput){
+            resultTextView.setTextColor(getResources().getColor(R.color.black));
+            resultTextView.setText(view.getTag().toString());
+            firstInput = false;
+        }
+        else{
+            String number = resultTextView.getText().toString().replace(",", "");
+            if(number.length() > 15){
+                return;
+            } else{
+                number = number + view.getTag().toString();
+                String getNumberString = calculatorClass.getDecimal(number);
+                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.O){
+                    if(getNumberString.length() > 13){
+                        resultTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 35);
+                    } else if(getNumberString.length() > 18){
+                        resultTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
+                    }
+                }
+                resultTextView.setText(getNumberString);
+            }
+        }
+    }
+
+
+    private void clearData() {
+        firstInput = true;
+        resultTextView.setTextColor(getResources().getColor(R.color.gray));
+        resultTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 50);
+        resultTextView.setText(CalculatorClass.CLEAR_INPUT);
     }
 }
